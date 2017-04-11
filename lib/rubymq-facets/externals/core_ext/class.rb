de47fc78480da0a85fbe27948414ea08187f2434
Stan Senotrusov <stan@senotrusov.com>
2017-04-11 14:32:51 +0200

    License

--- lib/rubymq-facets/externals/core_ext/class.rb
+++ lib/rubymq-facets/externals/core_ext/class.rb
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2004-2008 David Heinemeier Hansson
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
--- lib/rubymq-facets/externals/core_ext/time.rb
+++ lib/rubymq-facets/externals/core_ext/time.rb
@@ -1,4 +1,4 @@
-#
+
 #  Copyright (c) 2008 Ezra Zygmuntowicz
 #
 #  Permission is hereby granted, free of charge, to any person obtaining
